2930a6b1bd828eb0216d59092e3a8f1db87faecf
heavenxdior <86729192+HeavenRicks@users.noreply.github.com>
2021-07-01 17:33:40 +0000

    everything

--- primary.py
+++ primary.py
@@ -11,6 +11,9 @@
 #       1 - create a print statement that prints the sum of two numbers
 #       2 - create a print statement that prints the sum of three numbers
 #       3 - create a print statement the prints the sum of two negative numbers
+print(7+3)
+print(3+6+2)
+print(-5+-7)
 
 
 
@@ -19,7 +22,9 @@
 #       1 - create a print statement that prints the difference of two numbers
 #       2 - create a print statement that prints the difference of three numbers
 #       3 - create a print statement the prints the difference of two negative numbers
-
+print(10-4)
+print(24-8-3)
+print(-9--3)
 
 
 # multiplication and true division
@@ -27,25 +32,26 @@
 #       1 - create a print statement the prints the product of two numbers
 #       2 - create a print statement that prints the dividend of two numbers
 #       3 - create a print statement that evaluates an operation using both multiplication and division
-
-
+print(8*4)
+print(16/2)
+print(4*6/2)
 
 # floor division
 #   instructions
 #       1 - using floor division, print the dividend of two numbers.
-
+print(24//8)
 
 
 # exponentiation
 #   instructions
 #       1 - using exponentiation, print the power of two numbers
-
+print(18**2)
 
 
 # modulus
 #   instructions
 #       1 - using modulus, print the remainder of two numbers
-
+print(50%7)
 
 # --------------- Section 2 --------------- #
 # ---------- String Concatenation --------- #
@@ -55,15 +61,18 @@
 #       1 - print the concatenation of your first and last name
 #       2 - print the concatenation of five animals you like
 #       3 - print the concatenation of each word in a phrase
-
-
+print('Heaven' + 'Ricks')
+print('Dolphins'+'Pandas'+'Monkeys'+'Goats'+'Horse')
+print('i'+'love'+'dolphins')
 
 # duplication
 #   instructions
-#       1 - print the duplpication of your first 5 times
+#       1 - print the duplpication of your first name 5 times
 #       2 - print the duplication of a song you like 10 times
-
+print('heaven'*5)
+print('solid'*10)
 
 # concatenation and duplpication
 #   instructions
 #       1 - print the concatenation of two strings duplicated 3 times each
+print('Heaven'+'Ricks'*3)
